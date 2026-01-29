0ebab59d3b22b3890a6e524b76612b19484e72fa
Gaurav Dwivedi <gauravdwivedi.mail@gmail.com>
2026-01-29 14:45:41 +0530

    added some logs

--- Server/db/db.js
+++ Server/db/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 async function connect_db(){
     try {
         await mongoose.connect(process.env.MONGO_URI);
+       console.log("Database has  been connected");  
+
     } catch (error) {
         console.log("Database has not been connected");  
     }
