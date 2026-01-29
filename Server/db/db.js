3ca03055fce54697f3bb58ba0ee7baa1354d55e8
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2026-01-29 14:42:47 +0530

    Project basic setup has been completed

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 async function connect_db(){
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database has been connected");
+        
     } catch (error) {
+        console.log("Database has been not connected ",error.message);
         
     }
 }
+
+export default connect_db;
