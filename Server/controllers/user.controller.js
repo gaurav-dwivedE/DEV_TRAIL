99e40dac3126df09c429ad9a180a6f682f3f4bd3
Gaurav Dwivedi <gauravdwivedi.mail@gmail.com>
2026-01-29 18:06:48 +0530

    created login api

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,41 +1,80 @@
-import userModel from '../models/user.model.js'
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const register = async (req, res) => {
-    try {
-       const { name, email, password } = req.body || {}
-       const hashedPass = await bcrypt.hash(password, 10);
+  try {
+    const { name, email, password } = req.body || {};
+    const hashedPass = await bcrypt.hash(password, 10);
 
-       let user = await userModel.findOne({email});
-       
-       if(user){
-        return res.status(409).json({
-            message: "user is already registered"
-        })
-       }
+    let user = await userModel.findOne({ email });
 
-       user = await userModel.create({ name, email, password:hashedPass }).toObject();
+    if (user) {
+      return res.status(409).json({
+        message: "user is already registered",
+      });
+    }
+
+    user = await userModel
+      .create({ name, email, password: hashedPass })
       
-       const { password:_, ...userWithoutPass } = user;
+    const userWithoutPass  = user.toObject();
+    delete userWithoutPass.password;
 
-       const token = jwt.sign(
-        {
+    const token = jwt.sign(
+      {
         id: user._id,
         email: user.email,
-        }, 
-       process.env.JWT_SECRET);
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
 
-       res.status(201).json({ token,
-         user: userWithoutPass
+    res.status(201).json({ token, user: userWithoutPass, token });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
     });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({
-            error: error.message
-        })
-    } 
- }
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body || {};
 
- export default {register}
+    const user = await userModel.findOne({email}).lean();
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
 
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+    const { password: _, ...userWithoutPass } = user;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
+
+    res.status(200).json({
+        token,
+      user: userWithoutPass,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+export default { register, login };
--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -4,6 +4,7 @@ import userController from "../controllers/user.controller.js";
 const router = Router();
 
 router.post("/register", userController.register)
+router.post("/login", userController.login)
 
 
 export default router;
